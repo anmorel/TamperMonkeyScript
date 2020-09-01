d09d5bf7c874854bebb5c79c25f0a51c35ceddad
anmorel <morel.antoine@gmail.com>
2020-09-01 11:29:53 +0200

    Create medium.js

---
+++ medium.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Medium
+// @namespace    raymond876
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://medium.com/*
+// @match        https://itnext.io/*
+// @require https://code.jquery.com/jquery-2.1.1.min.js
+// @grant        GM_openInTab
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var $ = window.jQuery;
+
+    var link = $('#paywall-background-color h4 #paywall-upsell-button-upgrade a');
+    if ( typeof link !== "undefined" && link) {
+        link.html("Show FREE")
+        link.click(function(){
+            GM_openInTab(window.location.href, {
+                incognito: true
+            })
+            return false;
+        });
+    }
+})();
