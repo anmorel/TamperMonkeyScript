e81514c40a137cd0d4d151138fd9a45cf2a89a7d
anmorel <morel.antoine@gmail.com>
2020-09-06 10:42:51 +0200

    Update medium.js

--- medium.js
+++ medium.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Medium
 // @namespace    raymond876
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://medium.com/*
